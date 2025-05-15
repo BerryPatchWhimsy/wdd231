0a64ac3923df0f3c1c19badabf608b999da58907
BerryPatchWhimsy <berrypatchgirl@myyahoo.com>
2025-05-15 15:35:24 -0700

    chamber header and footer done

---
+++ chamber/scripts/header.js
@@ -0,0 +1,10 @@
+// This is a basic JavaScript file for the Chamber of Commerce website.
+const button = document.querySelector("#menu");
+const navigation = document.querySelector("#animateMe");
+
+button.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    button.classList.toggle("open");
+});
+
+document.querySelector("#name").innerHTML = "Carlin City";
