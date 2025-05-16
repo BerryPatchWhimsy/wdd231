23360acd7777bf4b7fd0ffad2fb0fb516c66ac93
BerryPatchWhimsy <berrypatchgirl@myyahoo.com>
2025-05-15 18:34:19 -0700

    chamber json

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,39 @@
+const membersCC = data/members.json;
+
+const cards = document.querySelector("#cards");
+
+async function getMemberData() {
+    const response = await fetch(membersCC);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+
+getMemberData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+
+        let name = document.createElement("h2");
+        name.textContent = member.businessName;
+        card.appendChild(name);
+
+        let address = document.createElement("p");
+        address.textContent = `${member.address}`;
+        card.appendChild(address);
+
+        let phone = document.createElement("p");
+        phone.textContent = member.phone;
+        card.appendChild(phone);
+
+        let picture = document.createElement("img");
+        picture.src = member.image;
+        picture.alt = name;
+        picture.loading = `lazy`;
+        picture.setAttribute('width', '300');
+        picture.setAttribute('height', '150');
+        card.appendChild(picture);
+
+        cards.appendChild(card);
+    });
+}
