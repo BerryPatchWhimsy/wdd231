b82b4f003c24c12461cf8584bd34f50fc65f1987
BerryPatchWhimsy <berrypatchgirl@myyahoo.com>
2025-05-23 17:14:31 -0700

    chamber home

--- chamber/scripts/forecast.js
+++ chamber/scripts/forecast.js
@@ -10,18 +10,18 @@ const icon1 = document.querySelector("#icon1");
 const icon2 = document.querySelector("#icon2");
 const icon3 = document.querySelector("#icon3");
 
-const apiKey = "fef381b00dd5f7ee47bf318e599f2bac";
-const latitude = 40.71;
-const longitude = -116.10;
-const units = "imperial";
+const APIKey = "fef381b00dd5f7ee47bf318e599f2bac";
+const lat = 40.71;
+const lon = -116.10;
+const uni = "imperial";
 
-const url = `//api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+const forecastURL = `//api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=${uni}`;
 
 
 
-async function apiFetch() {
+async function APIFetch() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(forecastURL);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
@@ -34,16 +34,31 @@ async function apiFetch() {
         console.log(error);
     }
 }
-apiFetch();
+APIFetch();
 
 function displayResults(data) {
-    const day1Data = data.list[0];
-    const day2Data = data.list[1];
-    const day3Data = data.list[2];
+    data.list.forEach((day) => {
+        if (day.dt_txt.includes("00:00:00")) {
+            const date = new Date(day.dt * 1000);
+            const options = { weekday: "long" };
+            const dayName = new Intl.DateTimeFormat("en-US", options).format(date);
 
-    temp1.innerHTML = `${day1Data.main.temp}&deg;F`;
-    temp2.innerHTML = `${day2Data.main.temp}&deg;F`;
-    temp3.innerHTML = `${day3Data.main.temp}&deg;F`;
+            const temperature = day.main.temp;
 
-    const iconSrc1 = `https://openweathermap.org/img/wn/${day1Data.weather[0]}@2x.png`;
+            const iconSrc = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2.png`;
+            const description = day.weather[0].description;
+
+            const forecastDay = document.querySelector(".days");
+
+            const dayContainer = document.createElement("div");
+            dayContainer.classList.add("daily");
+            dayContainer.innerHTML = `
+        <p>${dayName}, ${date.toLocaleDateString()}</p>
+        <img src="${iconSrc}" alt="${description}">
+        <p>${temperature}&deg;F</p>
+        `;
+
+            forecastDay.appendChild(dayContainer);
+        }
+    });
 }
